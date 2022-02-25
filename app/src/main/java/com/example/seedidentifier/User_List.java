c746b70a7591160309633d25f1412eb60f9831c6
MatthewMcNatt <77641967+MatthewMcNatt@users.noreply.github.com>
2022-02-25 12:36:10 -0600

    Delete User_List.java

--- app/src/main/java/com/example/seedidentifier/User_List.java
+++
@@ -1,9 +0,0 @@
-package com.example.seedidentifier;
-
-import java.io.Serializable;
-import java.util.List;
-
-public class User_List implements Serializable {
-    private static final long serialVersionUID = 1L;
-    public List<User> _users;
-}
