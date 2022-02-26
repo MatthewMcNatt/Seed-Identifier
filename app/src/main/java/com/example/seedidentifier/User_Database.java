5859b9f8e073a44db977695bf88f9338e8bd9aec
MatthewMcNatt <matthew.mcnatt1@gmail.com>
2022-02-26 15:57:05 -0600

    added some database functionality

--- app/src/main/java/com/example/seedidentifier/User_Database.java
+++ app/src/main/java/com/example/seedidentifier/User_Database.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -16,13 +17,25 @@ import java.util.List;
     -Matthew 2/25
 */
 public class User_Database {
-    private List<User> _users;
 
-    public void Load_data(String fileName) {
+    //fields
+    private ArrayList<User> _users;
+    private int user_Num;
+
+    //simple getters & setters
+    public int getUserNum(){return user_Num;}
+
+    //constructors
+    public User_Database(){
+        user_Num = 0;
+    }
+
+    //logic
+    public void loadData(String fileName) {
         try{
             FileInputStream fi = new FileInputStream(fileName);
             ObjectInputStream oi = new ObjectInputStream(fi);
-            _users = (List<User>) oi.readObject();
+            _users = (ArrayList<User>) oi.readObject();
             fi.close();
             oi.close();
 
@@ -35,12 +48,14 @@ public class User_Database {
 
     }
 
-    public void save_data(String fileName){
+    public void saveData(String fileName){
         try{
             FileOutputStream f = new FileOutputStream(fileName);
             ObjectOutputStream o = new ObjectOutputStream(f);
             o.writeObject(_users);
+            o.flush();
             o.close();
+            f.flush();
             f.close();
 
         }catch (FileNotFoundException e) {
@@ -50,6 +65,45 @@ public class User_Database {
         }
 
     }
+
+    /*get by name*/
+    public User getUser(String name){
+        for(User u : _users){
+            if (u.getUserName().equals(name)) return u;
+        }
+        return null;
+    }
+
+    /*standard get by index*/
+    public User getUser(int i){
+        return _users.get(i);
+    }
+
+    /*gets user index by name*/
+    public int getUserIndex(String name){
+        for(int i = 0; i < _users.size(); i++){
+            if(_users.get(i).getUserName().equals(name)) return i;
+        }
+        return 0;
+    }
+
+    /*standard add and increment*/
+    public void addUser(User user){
+        _users.add(user);
+        user_Num++;
+    }
+
+    /*standard remove and decrement*/
+    public void removeUser(int key){
+        _users.remove(key);
+        user_Num--;
+    }
+
+    /*replaces user at i with User u */
+    public void editUser(int i, User u){
+        _users.set(i, u);
+    }
+
 
 
 
