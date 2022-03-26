8e7ab88e71c37e51cc0ea4e96074b8764b73764a
Shane <shane.purdy@mavs.uta.edu>
2022-03-25 19:16:36 -0500

    Unsure, commented out changes. Will ask questions about stuff before uncommenting them out.

--- app/src/main/java/com/example/seedidentifier/MainActivity.java
+++ app/src/main/java/com/example/seedidentifier/MainActivity.java
@@ -12,11 +12,15 @@ import org.w3c.dom.Text;
 //a check
 // repo confirmation
 public class MainActivity extends AppCompatActivity {
-Button SignUp;
-Button SignIn;
-EditText EnterPassword;
-EditText EnterUsername;
-Text textView;
+
+    Button SignUp;
+    Button SignIn;
+    EditText EnterPassword;
+    EditText EnterUsername;
+    Text textView;
+    //User_Database users = new User_Database();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/seedidentifier/Signup.java
+++ app/src/main/java/com/example/seedidentifier/Signup.java
@@ -3,20 +3,27 @@ package com.example.seedidentifier;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Signup extends AppCompatActivity {
 
-   Button CreateAccount;
+    Button CreateAccount;
+    //EditText NewPassword;
+    //EditText NewUsername;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        //NewPassword = (EditText)findViewById(R.id.NewPassword);
+        //NewUsername = (EditText)findViewById(R.id.NewUsername);
 
         CreateAccount = (Button)findViewById(R.id.CreateAccount);
         CreateAccount.setOnClickListener(view -> {
+            //users.addUser(new Default_User());
             Intent i = new Intent(Signup.this, MenuNavigation.class);
             startActivity(i);
         });
