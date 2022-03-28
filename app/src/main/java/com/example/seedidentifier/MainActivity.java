cb7421df31a57fe447a29c66a7d11d7a21f7472d
Shane <shane.purdy@mavs.uta.edu>
2022-03-27 20:07:26 -0500

    Signup and signin, but not tested due to issues with gradle

--- app/src/main/java/com/example/seedidentifier/MainActivity.java
+++ app/src/main/java/com/example/seedidentifier/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ComponentActivity;
 
 import org.w3c.dom.Text;
 
@@ -17,9 +19,9 @@ public class MainActivity extends AppCompatActivity {
     Button SignIn;
     EditText EnterPassword;
     EditText EnterUsername;
-    Text textView;
-    //User_Database users = new User_Database();
-
+    //Text textView;
+    // Create a variable for the user database
+    User_Database users = new User_Database();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +30,33 @@ public class MainActivity extends AppCompatActivity {
         SignUp = (Button)findViewById(R.id.SignUp);
         SignUp.setOnClickListener(view -> {
             Intent i = new Intent(MainActivity.this,Signup.class);
+            i.putExtra("UserDatabase",users); // Sending Database to Signup activity
             startActivity(i);
         });
-
+        // Creating an arraylist to hold all of the current users
+        int index = 0;
+        ArrayList<User> userList = new ArrayList<User>();
+        while(users.getUser(index) != null)
+        {
+            userList.add(users.getUser(index));
+            index++;
+        }
         SignIn = (Button)findViewById(R.id.SignIn);
         SignIn.setOnClickListener(view -> {
-            Intent i = new Intent(MainActivity.this, MenuNavigation.class);
-            startActivity(i);
+            // Check through each and every user to see if the login information matches the entered information
+            for(User it : userList)
+            {
+                if(it.getUserName().equals(EnterUsername.getText().toString()))
+                {
+                    if(it.getUserPassword().equals(EnterPassword.getText().toString()))
+                    {
+                        // Now that the login information was verified, send the correct user to the menu navigation activity
+                        Intent i = new Intent(MainActivity.this, MenuNavigation.class);
+                        i.putExtra("User",it);
+                        startActivity(i);
+                    }
+                }
+            }
         });
 
     }
--- app/src/main/java/com/example/seedidentifier/Signup.java
+++ app/src/main/java/com/example/seedidentifier/Signup.java
@@ -10,21 +10,26 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Signup extends AppCompatActivity {
 
     Button CreateAccount;
-    //EditText NewPassword;
-    //EditText NewUsername;
+    EditText NewPassword;
+    EditText NewUsername;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        //NewPassword = (EditText)findViewById(R.id.NewPassword);
-        //NewUsername = (EditText)findViewById(R.id.NewUsername);
-
+        NewPassword = (EditText)findViewById(R.id.NewPassword);
+        NewUsername = (EditText)findViewById(R.id.NewUsername);
         CreateAccount = (Button)findViewById(R.id.CreateAccount);
+        // Get the user database from the main activity
+        Intent in = getIntent();
+        User_Database users = (User_Database)in.getSerializableExtra("UserDatabase");
         CreateAccount.setOnClickListener(view -> {
-            //users.addUser(new Default_User());
+            // Add the entered input as a new default user
+            users.addUser(new Default_User(NewUsername.getText().toString(), NewPassword.getText().toString()));
             Intent i = new Intent(Signup.this, MenuNavigation.class);
+            // Send the user database to the menu navigation activity
+            i.putExtra("UserDatabase",users);
             startActivity(i);
         });
     }
