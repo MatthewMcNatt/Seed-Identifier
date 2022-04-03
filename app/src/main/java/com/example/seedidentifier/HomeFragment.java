44b6da9fc6a83bd02303fa0bfcf4db0a9cbb883e
David Nguyen <brown87434@gmail.com>
2022-04-03 17:02:59 -0500

    Added onItemClick to listView

--- app/src/main/java/com/example/seedidentifier/HomeFragment.java
+++ app/src/main/java/com/example/seedidentifier/HomeFragment.java
@@ -1,19 +1,24 @@
 package com.example.seedidentifier;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 
-public class HomeFragment extends Fragment implements View.OnClickListener {
+public class HomeFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,6 +50,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         seed_populator.populate(seed_database);
 
         SeedList = seed_database.get_seeds();
+
         //  System.out.println(SeedList);
 //
 //        SeedList.add(new Seed("Sunflower Seed","Sunflowers make for a beautiful edition to any summer garden",
@@ -55,8 +61,22 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 //                R.drawable.cucumber));
 //        SeedList.add(new Seed("Pumpkin Seed","This staple vegetable is technically a fruit that grows from a vine and does best in warm humid environments",
 //                R.drawable.sunflower));
-
         SeedAdapter adapter = new SeedAdapter(SeedList,getActivity());
+
+        ArrayList<Seed> finalSeedList = SeedList;
+        SeedListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getActivity(),Seed_Info.class);
+                int position = 0;
+                intent.putExtra("SEED_IMAGE", finalSeedList.get(i).getImage());
+                intent.putExtra("SEED_NAME", finalSeedList.get(i).getSeedName());
+                intent.putExtra("SEED_DESCRIPTION", finalSeedList.get(i).getDescription());
+
+
+                startActivity(intent);
+            }
+        });
         SeedListView.setAdapter(adapter);
 
         return contentView;
--- app/src/main/java/com/example/seedidentifier/SeedAdapter.java
+++ app/src/main/java/com/example/seedidentifier/SeedAdapter.java
@@ -8,12 +8,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import java.util.ArrayList;
 
 public class SeedAdapter extends ArrayAdapter<Seed> {
 
     ArrayList<Seed> seedList;
     Context mContext;
+
 
     public SeedAdapter(ArrayList<Seed> seed, Context context) {
         super(context, R.layout.list_view_row,seed);
@@ -25,25 +28,31 @@ public class SeedAdapter extends ArrayAdapter<Seed> {
         ImageView SeedImage;
         TextView SeedName;
         TextView SeedDescription;
+
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         MyViewHolder holder = new MyViewHolder();
         // Get the data item for this position
-        Seed seed = getItem(position);
+
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_view_row, parent, false);
         }
+
+        Seed seed = getItem(position);
         // Lookup view for data population
         holder.SeedImage = (ImageView) convertView.findViewById(R.id.SeedImage);
         holder.SeedName = (TextView) convertView.findViewById(R.id.SeedName);
         holder.SeedDescription = (TextView) convertView.findViewById(R.id.SeedDescription);
+
+
         // Populate the data into the template view using the data object
         holder.SeedImage.setImageResource(seed.getImage());
         holder.SeedName.setText(seed.getSeedName());
         holder.SeedDescription.setText(seed.getDescription());
+
 
         // Return the completed view to render on screen
         return convertView;
