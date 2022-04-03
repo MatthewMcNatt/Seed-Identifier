11aa32d77f7caa9f67efeb0beee947d3d7b09667
David Nguyen <brown87434@gmail.com>
2022-04-03 17:06:02 -0500

    Added onItemClick to listView

---
+++ app/src/main/java/com/example/seedidentifier/Seed_Info.java
@@ -0,0 +1,49 @@
+package com.example.seedidentifier;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Seed_Info extends AppCompatActivity {
+
+    ImageView SeedInfoPicture;
+    EditText SeedInfoName, SeedInfoDescription, SeedInfoNotes;
+    Button OK;
+    int id;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seed_info);
+
+        int SeedImage = getIntent().getIntExtra("SEED_IMAGE",0);
+        String SeedName = getIntent().getStringExtra("SEED_NAME");
+        String SeedDescription = getIntent().getStringExtra("SEED_DESCRIPTION");
+
+        SeedInfoPicture = findViewById(R.id.IV_SeedPicture);
+        SeedInfoName = findViewById(R.id.ET_SeedName);
+        SeedInfoDescription = findViewById(R.id.ET_SeedDescription);
+        SeedInfoNotes = findViewById(R.id.ET_SeedNotes);
+
+        SeedInfoPicture.setImageResource(SeedImage);
+        SeedInfoName.setText(SeedName);
+        SeedInfoDescription.setText(SeedDescription);
+
+
+        OK = findViewById(R.id.OK_Btn);
+        OK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Seed_Info.this,MenuNavigation.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
